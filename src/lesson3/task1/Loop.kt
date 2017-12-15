b37ef05324d4af9832b3cbaa96724ef24533e408
Alexey Sinitsin <31938442+ONahara@users.noreply.github.com>
2017-12-15 22:12:21 +0300

    qwerty

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -152,13 +152,7 @@ fun isCoPrime(m: Int, n: Int): Boolean = lcm(m, n) == m * n
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean {
-    for (q in m..n) {
-        if (q == 0 || q == 1) return true
-        if (q.toDouble() % Math.sqrt(q.toDouble()) == 0.0) return true
-    }
-    return false
-}
+fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
 
 /**
  * Средняя
